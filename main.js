8a21f1974f13cc45ab3c46b260128458c35a095b
Gudmunduro <goh140400@gmail.com>
2019-05-02 23:25:49 +0000

    chat send, ui

--- main.js
+++ main.js
@@ -5,6 +5,20 @@ class User {
         return localStorage.getItem("token");
     }
 
+    static async loggedIn() {
+        if (!this.token) return false;
+        axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.token;
+
+        try {
+            const response = await axios.get("http://localhost:8080/user/test");
+            console.log(response);
+            return response.status === 200;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
     static async login (username, password) {
         try {
             const response = await axios.get("http://localhost:8080/user/login", {
@@ -12,8 +26,9 @@ class User {
                     username,
                     password 
                 }
-            })
+            });
             localStorage.setItem("token", response.data.token);
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + response.data.token;
         } catch (error) {
             // TODO: Do real errors
             alert("Login failed");
@@ -32,6 +47,15 @@ class Chat {
         this.socket.onopen = this.onOpen;
         this.socket.onmessage = this.onMessage;
         this.socket.onclose = this.onClose;
+
+        document.getElementById('sendButton').addEventListener('click', this.onSendButton.bind(this));
+    }
+
+    onSendButton(e) {
+        const text = document.getElementById("sendInput").value;
+        if (text == "") return;
+
+        this.socket.send(text);
     }
 
     onOpen(e) {
@@ -47,3 +71,14 @@ class Chat {
     }
 
 }
+
+window.addEventListener('load', e => {
+    User.loggedIn().then(loggedIn => {
+        if (loggedIn) {
+            console.log("logged in")
+            window.chatInstance = new Chat();
+        } else {
+            console.log("not logged in");
+        }
+    });
+});
